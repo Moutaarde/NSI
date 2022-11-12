dbbf886bf06200e1f9fdd3364670f2b1901deea1
Moutarde <112987181+Moutaarde@users.noreply.github.com>
2022-11-12 19:09:32 +0100

    Add files via upload

--- Projet.sur.les.images/warhol.py
+++ Projet.sur.les.images/warhol.py
@@ -6,10 +6,10 @@ for y in range(hauteur):
         r,v,b=img.getpixel((x,y))
         #img.putpixel((x,y),(v,b,r))
         #img.putpixel((x,y),(b,r,v))
-        #if r>v and r>b:
+        #if r>v and r>b:#filtre vert
             #img.putpixel((x,y),(0,v,b))
         #if v>r and v>b:
-            #img.putpixel((x,y),(r,0,b))
+            #mg.putpixel((x,y),(r,0,b))
         #if b>v and b>r:
             #img.putpixel((x,y),(r,v,0))
 img.show()
